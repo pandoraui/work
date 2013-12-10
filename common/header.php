672cef0c2a54a34443841c181605ef70a9b4318d
cloudAi <1395093509@qq.com>
2013-12-10 11:20:10 +0800

    添加页面

---
+++ common/header.php
@@ -0,0 +1,12 @@
+
+
+<?php include("common/tempnav.html"); ?>
+
+<!-- 公共头部开始  -->
+<div class="header clearfix hide">
+	<span class="hotline">1010-6060</span>
+	<a class="logo" href="http://www.lvmama.com/">驴妈妈旅游网</a>
+	<div class="adbox"></div>
+</div> <!--header end-->
+
+<!-- 公共头部结束  -->
---
+++ common/meta.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?php echo $title; ?></title>
+<link rel="shortcut icon" type="image/x-icon" href="http://www.lvmama.com/favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="页面关键字">
+<meta name="description" content="页面描述">
+
+<!-- 生产线使用样式
+
+// 通用样式
+<link rel="stylesheet" href="http://pic.lvmama.com/min/index.php?f=/styles/v3/pa-base.css,/styles/v3/pa-common.css,/styles/v3/pa-modules.css" >
+
+// 页面样式
+<link rel="stylesheet" href="propage.css">
+
+-->
+
+<!-- 预览页面样式 -->
+<link rel="stylesheet" href="/labs/assets/css/pa-base.css" >
+<link rel="stylesheet" href="/labs/v4/css/lv-common.css" >
+<link rel="stylesheet" href="/labs/assets/css/pa-modules.css" >
+
+<!-- propage.css 项目页面样式 -->
+<link rel="stylesheet" href="/labs/css/propage.css" >
