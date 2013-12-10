78dd32ff78ef09583524bdf736938fd3eb21b240
cloudAi <1395093509@qq.com>
2013-12-10 13:41:06 +0800

    修改

--- common/header.php
+++ common/header.php
@@ -4,9 +4,9 @@
 
 <!-- 公共头部开始  -->
 <div class="header clearfix hide">
-	<span class="hotline">1010-6060</span>
-	<a class="logo" href="http://www.lvmama.com/">驴妈妈旅游网</a>
-	<div class="adbox"></div>
+    <span class="hotline">1010-6060</span>
+    <a class="logo" href="http://www.lvmama.com/">驴妈妈旅游网</a>
+    <div class="adbox"></div>
 </div> <!--header end-->
 
 <!-- 公共头部结束  -->
