ef1efdf49c95693ced18a9e5bf422a207ea01936
cloudyan <1395093509@qq.com>
2014-11-21 11:20:14 +0800

    修改

--- frame/common/meta.php
+++ frame/common/meta.php
@@ -1,5 +1,5 @@
 
-<link rel="shortcut icon" type="image/x-icon" href="http://www.lvmama.com/favicon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
