7e10ce5c84c139f64035aeed2f68c29e1bb022cc
cloudyan <1395093509@qq.com>
2014-12-10 20:49:27 +0800

    开始好项目

---
+++ hao/common/meta.php
@@ -0,0 +1,17 @@
+
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- 开发的童鞋注意啦，这里是生产线使用样式，你会用到的
+
+<link rel="stylesheet" href="http://pic.lvmama.com/min/index.php?f=/styles/v5/base.css,/styles/v5/common.css" >
+<link rel="stylesheet" href="http://pic.lvmama.com/min/index.php?f=/styles/v5/modules/dialog.css,/styles/v5/modules/table.css,/styles/v5/modules/arrow.css,/styles/v5/modules/form.css,/styles/v5/modules/button.css,/styles/v5/modules/paging.css,/styles/v5/modules/tip.css" >
+<link rel="stylesheet" href="http://pic.lvmama.com/min/index.php?f=styles/v5/propage.css">
+
+-->
+
+<!-- 下面这是预览页面使用的样式，不要上线 -->
+<link rel="stylesheet" href="static/css/base.css" >
+<link rel="stylesheet" href="static/css/modules.css" >
+<link rel="stylesheet" href="css/propage.css" >
---
+++ hao/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>猜你喜欢的导航站</title>
+<meta name="keywords" content="页面关键字">
+<meta name="description" content="页面描述">
+<?php include("common/meta.php"); ?>
+</head>
+<body>
+<?php include("common/header.php"); ?>
+<div class="wrap">
+    
+</div>
+
+
+<?php include("common/footer.php"); ?>
+
+</body>
+</html>
