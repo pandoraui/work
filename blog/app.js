f03688fa92eeb9bb3440eb2ce8c7df1c019620db
cloudAi <cloudai@qq.com>
2014-12-17 23:31:58 +0800

    first update

--- blog/app.js
+++ blog/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
@@ -21,6 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+routes(app);
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 app.use('/', routes);
 app.use('/users', users);
