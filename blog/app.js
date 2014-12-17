0f3c2598c8f86ae0b11ae99509f59bfd4837ab66
cloudAi <cloudAi@users.noreply.github.com>
2014-12-18 00:06:43 +0800

    Update app.js

--- blog/app.js
+++ blog/app.js
@@ -11,6 +11,8 @@ var users = require('./routes/users');
 var routes = require('./routes/index');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
