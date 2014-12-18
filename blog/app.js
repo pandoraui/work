8cdd76ddf4f5f229b4e96d105f353501e3f36fde
cloudAi <cloudai@qq.com>
2014-12-18 12:41:47 +0800

    update

--- blog/app.js
+++ blog/app.js
@@ -5,12 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+//var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var routes = require('./routes/index');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
